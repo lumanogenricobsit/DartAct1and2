727c8fbd2510e399a0852f8f4d1556993a0e0fa7
lumanogenricobsit <enrico.lumanog1970@gmail.com>
2025-08-08 10:00:45 +0000

    Activity 1 : Understanding the Dart

---
+++ Act1.dart
@@ -0,0 +1,11 @@
+void main () {
+ String greeting = "WElCOME";
+ 
+ String user = "Alice!";
+
+ print("$greeting, $user");
+
+ dynamic userDynamic = 25;
+
+ print("$greeting, $userDynamic!");
+}
---
+++ Act2.dart
@@ -0,0 +1,19 @@
+void main() {
+  
+  double rectanglePerimeter = calculatePerimeter(10, 5);
+  print("Perimeter of rectangle: $rectanglePerimeter");
+
+
+  double squarePerimeter = calculatePerimeter(7, 0, isSquare: true);
+  print("Perimeter of square: $squarePerimeter");
+}
+
+double calculatePerimeter(double width, double height, {bool isSquare = false}) {
+  if (isSquare) {
+    
+    return 4 * width;
+  } else {
+    
+    return 2 * (width + height);
+  }
+}
